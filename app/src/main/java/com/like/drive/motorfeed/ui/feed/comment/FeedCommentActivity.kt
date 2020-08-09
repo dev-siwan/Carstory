904a6472cde88fca75847c23e3dbee78aa5cee93
Dev.Kimsiwan <dev.ksw@gmail.com>
2020-08-10 00:00:16 +0900

    코멘트 페이지 생성

---
+++ app/src/main/java/com/like/drive/motorfeed/ui/feed/comment/FeedCommentActivity.kt
@@ -0,0 +1,11 @@
+package com.like.drive.motorfeed.ui.feed.comment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.like.drive.motorfeed.R
+import com.like.drive.motorfeed.databinding.ActivityFeedCommentBinding
+import com.like.drive.motorfeed.ui.base.BaseActivity
+
+class FeedCommentActivity : BaseActivity<ActivityFeedCommentBinding>(R.layout.activity_feed_comment) {
+
+}
