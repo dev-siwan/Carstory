0027d6f77d1494e4051aee7bccd7100f944c0ef0
Dev.Kimsiwan <dev.ksw@gmail.com>
2020-09-16 00:53:39 +0900

    포맷데이터

--- app/src/main/java/com/like/drive/motorfeed/fcm/AppFireMessagingService.kt
+++ app/src/main/java/com/like/drive/motorfeed/fcm/AppFireMessagingService.kt
@@ -10,9 +10,7 @@ import com.like.drive.motorfeed.common.define.FirebaseDefine
 import com.like.drive.motorfeed.common.user.UserInfo
 import com.like.drive.motorfeed.data.notification.NotificationSendData
 import com.like.drive.motorfeed.data.notification.NotificationType
-import com.like.drive.motorfeed.pref.UserPref
 import com.like.drive.motorfeed.repository.notification.NotificationRepository
-import com.like.drive.motorfeed.ui.base.ext.convert
 import com.like.drive.motorfeed.ui.base.ext.toDataClass
 import com.like.drive.motorfeed.util.notification.NotificationUtil
 import kotlinx.coroutines.CoroutineScope
@@ -21,23 +19,20 @@ import kotlinx.coroutines.cancel
 import kotlinx.coroutines.launch
 import org.koin.core.KoinComponent
 import org.koin.core.inject
-import timber.log.Timber
 
 class AppFireMessagingService : FirebaseMessagingService(), KoinComponent {
 
     val context = MotorFeedApplication.getContext()
     private val repo: NotificationRepository by inject()
 
-    override fun onNewToken(token: String) {
-
-        super.onNewToken(token)
+    override fun onNewToken(p0: String) {
+        super.onNewToken(p0)
     }
 
     override fun onMessageReceived(remoteMessaging: RemoteMessage) {
         super.onMessageReceived(remoteMessaging)
 
         if (UserInfo.userInfo == null) return
-
 
         remoteMessaging.data.toDataClass<NotificationSendData>()?.let {
 
