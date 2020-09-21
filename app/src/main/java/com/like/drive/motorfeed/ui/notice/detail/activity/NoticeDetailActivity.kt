bae468fe7431e63319b11da114d28bb1fe0c3df1
station3-kimsiwan <62864137+station3-kimsiwan@users.noreply.github.com>
2020-09-21 19:01:33 +0900

    인터샙트 변경

--- app/src/main/java/com/like/drive/motorfeed/ui/notice/detail/activity/NoticeDetailActivity.kt
+++ app/src/main/java/com/like/drive/motorfeed/ui/notice/detail/activity/NoticeDetailActivity.kt
@@ -1,13 +1,17 @@
 package com.like.drive.motorfeed.ui.notice.detail.activity
 
 import android.os.Bundle
-import androidx.lifecycle.Observer
 import com.like.drive.motorfeed.R
+import com.like.drive.motorfeed.common.define.GitDefine
 import com.like.drive.motorfeed.data.notice.NoticeData
 import com.like.drive.motorfeed.databinding.ActivityNoticeDetailBinding
 import com.like.drive.motorfeed.ui.base.BaseActivity
 import com.like.drive.motorfeed.ui.notice.detail.viewmodel.NoticeDetailViewModel
+import es.dmoral.markdownview.Config
 import kotlinx.android.synthetic.main.activity_notice_detail.*
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.Response
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class NoticeDetailActivity :
@@ -23,28 +27,37 @@ class NoticeDetailActivity :
         intent.getParcelableExtra<NoticeData>(NOTICE_DATA_KEY)?.let {
             noticeData = it
             binding?.data = it
-            viewModel.getFile(it)
+            //viewModel.getFile(it)
+
+            webView.run {
+                setCurrentConfig(Config.getDefaultConfig().apply {
+                    defaultOkHttpClient =
+                        OkHttpClient().newBuilder().addInterceptor(CustomInterceptor()).build()
+                })
+
+                loadFromUrl("${GitDefine.GIT_BASE_URL}notice/${it.mdFile}")
+            }
+
         }
 
-        withViewModel()
-
     }
 
-    private fun withViewModel() {
-        with(viewModel) {
-            convertContent()
-        }
 
-    }
-
-    private fun NoticeDetailViewModel.convertContent() {
-        mdFileContent.observe(this@NoticeDetailActivity, Observer {
-            webView.loadFromText(it)
-        })
-    }
 
     companion object {
         const val NOTICE_DATA_KEY = "NoticeDataKey"
     }
 
+    inner class CustomInterceptor : Interceptor {
+
+        override fun intercept(chain: Interceptor.Chain): Response {
+            val original = chain.request()
+
+            val requestBuilder = original.newBuilder()
+                .header(GitDefine.HEADER_AUTH, GitDefine.GIT_TOKEN)
+                .build()
+
+            return chain.proceed(requestBuilder)
+        }
+    }
 }
