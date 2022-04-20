4eb08505a9b923c701b0db7a540c43d0786fb5fa
kimsiwan <59994769+dev-siwan@users.noreply.github.com>
2022-04-20 14:09:52 +0900

    refactor : firebaseModule  힐트 적용

---
+++ app/src/main/java/com/like/drive/carstory/di/FirebaseModule.kt
@@ -0,0 +1,38 @@
+package com.like.drive.carstory.di
+
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
+import com.like.drive.carstory.remote.common.FireBaseTask
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object FirebaseModule {
+    private val fireStoreInstance by lazy { FirebaseFirestore.getInstance() }
+    private val fireStorageInstance by lazy { FirebaseStorage.getInstance() }
+    private val fireBaseTask by lazy { FireBaseTask() }
+    private val fireBaseAuth by lazy { FirebaseAuth.getInstance() }
+
+    @Singleton
+    @Provides
+    fun provideFireStore() = fireStoreInstance
+
+    @Singleton
+    @Provides
+    fun provideFireStorage() = fireStorageInstance
+
+    @Singleton
+    @Provides
+    fun provideFirebaseAuth() = fireBaseAuth
+
+    @Singleton
+    @Provides
+    fun provideFirebaseTask() = fireBaseTask
+
+}
+
